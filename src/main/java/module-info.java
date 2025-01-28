e2951cf75f52f25199738b533a178ae806ca1a06
kubag5 <79789131+kubag5@users.noreply.github.com>
2025-01-28 19:30:55 +0100

    Dodanie znikniętej biblioteki

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,10 @@
 module pl.kubag5.demo {
     requires javafx.controls;
     requires javafx.fxml;
-<<<<<<< HEAD
     requires java.sql;
-=======
     requires org.apache.poi.poi;
-    requires org.jsoup;
     requires org.apache.poi.ooxml;
->>>>>>> feature/add-records
-
+    requires org.jsoup;
 
     opens pl.kubag5.demo to javafx.fxml;
     exports pl.kubag5.demo;
